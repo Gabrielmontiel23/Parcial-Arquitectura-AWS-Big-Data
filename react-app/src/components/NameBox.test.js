7fa1d83584b514ee7d46c370ad3fa9fb1c7b6eba
Jonathan <99345719+JonathanAgudelo02@users.noreply.github.com>
2024-10-28 15:07:36 -0500

    Update NameBox.test.js

--- react-app/src/components/NameBox.test.js
+++ react-app/src/components/NameBox.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import NameBox from './components/NameBox';
+import NameBox from './NameBox';
 
 test('renderiza el componente NameBox', () => {
   render(<NameBox />);
