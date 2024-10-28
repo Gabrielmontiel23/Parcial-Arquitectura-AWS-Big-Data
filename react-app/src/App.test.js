e7deefbe23103548f2eb456043af1b924737ac17
Gabriel Montiel <77250733+Gabrielmontiel23@users.noreply.github.com>
2024-10-28 13:29:34 -0500

    Update App.test.js

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+// src/App.test.js
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renderiza correctamente la caja de nombres y el botón para ver películas', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  // Verificar que se renderiza el componente de la caja de nombres
+  expect(screen.getByText('Gestión de Rentas')).toBeInTheDocument();
+
+  // Verificar el botón para alternar entre ver películas y registrar rentas
+  const toggleButton = screen.getByText('Ver Peliculas por Usuario');
+  expect(toggleButton).toBeInTheDocument();
+
+  // Simular un clic en el botón para alternar la vista
+  fireEvent.click(toggleButton);
+  expect(screen.getByText('Registrar Una Nueva Renta')).toBeInTheDocument();
+});
+
+test('renderiza el formulario de registro', () => {
+  render(<App />);
+
+  // Verificar que el formulario de registro esté en el DOM
+  expect(screen.getByText('Registrar Una Nueva Renta')).toBeInTheDocument();
 });
